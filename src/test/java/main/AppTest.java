9da9917ea42fbed0b3c4f9ab86337a24e024f099
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-05-27 00:25:36 +0200

    Added : documentation and SwingFX
    SwingFX : test taking a screenshot while executing the test

--- src/test/java/main/AppTest.java
+++ src/test/java/main/AppTest.java
@@ -1,11 +1,25 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -29,15 +43,24 @@ public class AppTest extends ApplicationTest{
 
     @Test
     public void should_contain_button_with_text() {
-        Assertions.assertThat(button).hasText("click me!");
+        Assertions.assertThat(button).hasText("click me!");              
     }
 
     @Test
     public void when_button_is_clicked_text_changes() {
         // when:
-        clickOn(".button");
-
+        clickOn(".button");        
         // then:
-        Assertions.assertThat(button).hasText("clicked!");
+        Assertions.assertThat(button).hasText("clicked!");        
+        
+        Image img = capture(button).getImage();
+                
+        
+        File outputfile = new File("saved.png");
+        try {
+			ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
