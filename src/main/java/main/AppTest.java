67390ee26a6de587c5a95c48e4ef88dbf85cfa4f
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-05-26 23:31:33 +0200

    Initial commit : setup to execute testFX : ok

---
+++ src/main/java/main/AppTest.java
@@ -0,0 +1,37 @@
+package main;
+
+import org.junit.Test;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class AppTest extends ApplicationTest{
+
+    private Button button;
+
+    @Override
+    public void start(Stage stage) {
+        button = new Button("click me!");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+        stage.setScene(new Scene(new StackPane(button), 100, 100));
+        stage.show();
+    }
+
+    @Test
+    public void should_contain_button_with_text() {
+        Assertions.assertThat(button).hasText("click me!");
+    }
+
+    @Test
+    public void when_button_is_clicked_text_changes() {
+        // when:
+        clickOn(".button");
+
+        // then:
+        Assertions.assertThat(button).hasText("clicked!");
+    }
+}
